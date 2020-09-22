7ea57dfa9777a6a228eae413178d83657f12002b
Zachary Wander <zachary.wander@gmail.com>
2020-09-22 10:31:04 -0400

    Fix layout issues

--- app/src/main/java/tk/zwander/galaxyunlock/MainActivity.kt
+++ app/src/main/java/tk/zwander/galaxyunlock/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.ContextAmbient
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
@@ -24,9 +23,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 import androidx.ui.tooling.preview.Preview
 import com.google.android.material.composethemeadapter.MdcTheme
 
@@ -54,7 +51,8 @@ class MainActivity : AppCompatActivity() {
 fun createFAQItems(): List<FAQItem> {
     return listOf(
         FAQItem(R.string.cost_title, R.string.cost_desc),
-        FAQItem(R.string.how_to_pay_title, R.string.how_to_pay_desc,
+        FAQItem(
+            R.string.how_to_pay_title, R.string.how_to_pay_desc,
             ButtonInfo(R.string.venmo_link, "https://venmo.com/")
         ),
         FAQItem(R.string.when_to_pay_title, R.string.when_to_pay_desc),
@@ -64,7 +62,8 @@ fun createFAQItems(): List<FAQItem> {
         FAQItem(R.string.needed_tools_title, R.string.needed_tools_desc),
         FAQItem(R.string.permanence_title, R.string.permanence_desc),
         FAQItem(R.string.wait_time_title, R.string.wait_time_desc),
-        FAQItem(R.string.other_questions_title, R.string.other_questions_desc,
+        FAQItem(
+            R.string.other_questions_title, R.string.other_questions_desc,
             ButtonInfo(R.string.telegram_group, "https://t.me/joinchat/EkE57lDUTDaP4b9jDlE4-Q")
         ),
     )
@@ -84,128 +83,48 @@ fun Preview() {
 @Composable
 fun MainContent() {
     MdcTheme(setTextColors = true) {
-        Column(
-            modifier = Modifier.fillMaxHeight()
-                .fillMaxWidth(),
-        ) {
+        val showVenmoEmailDialog = remember {
+            mutableStateOf(false)
+        }
+
+        VenmoDialog(showVenmoEmailDialog)
+
+        Column {
             TopAppBar(
                 title = {
                     Text(stringResource(id = R.string.app_name))
                 },
                 backgroundColor = MaterialTheme.colors.primary,
             )
-            ConstraintLayout(
-                modifier = Modifier.padding(PaddingValues(16.dp))
-                    .fillMaxHeight(),
-                constraintSet = ConstraintSet {
-                    val listRef = createRefFor("faq_list")
-                    val requestRef = createRefFor("request_unlock")
-                    val infoRef = createRefFor("device_info")
 
-                    constrain(listRef) {
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        top.linkTo(parent.top)
-                        bottom.linkTo(requestRef.top)
-                    }
-
-                    constrain(requestRef) {
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        top.linkTo(listRef.bottom)
-                        bottom.linkTo(infoRef.top)
-                    }
-
-                    constrain(infoRef) {
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        top.linkTo(requestRef.bottom)
-                        bottom.linkTo(parent.bottom)
-                    }
-                }
+            Column(
+                modifier = Modifier.padding(
+                    top = 8.dp,
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 4.dp
+                )
+                    .weight(1f),
             ) {
                 LazyColumnFor(
                     items = createFAQItems(),
                     itemContent = { item ->
                         FAQDialogCard(info = item)
                     },
-                    modifier = Modifier.layoutId("faq_list"),
                 )
+            }
 
-                val context = ContextAmbient.current
-                val showVenmoEmailDialog = remember {
-                    mutableStateOf(false)
-                }
-                val enteredEmail = remember {
-                    mutableStateOf("")
-                }
-
-                if (showVenmoEmailDialog.value) {
-                    AlertDialog(
-                        onDismissRequest = {},
-                        title = {
-                            Text(
-                                getString(R.string.enter_venmo_email),
-                                style = TextStyle(
-                                    fontSize = 18.sp,
-                                    fontWeight = FontWeight.Bold,
-                                ),
-                            )
-                        },
-                        text = {
-                            TextField(
-                                value = enteredEmail.component1(),
-                                keyboardType = KeyboardType.Email,
-                                onValueChange = {
-                                    enteredEmail.value = it
-                                }
-                            )
-                        },
-                        confirmButton = {
-                            Button(
-                                onClick = {
-                                    if (Patterns.EMAIL_ADDRESS.matcher(enteredEmail.value).matches()) {
-                                        context.sendRequest(enteredEmail.value)
-                                        enteredEmail.value = ""
-                                        showVenmoEmailDialog.value = false
-                                    } else {
-                                        Toast.makeText(
-                                            context,
-                                            R.string.enter_venmo_email,
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                    }
-                                },
-                                backgroundColor = Color.Transparent,
-                                elevation = 0.dp,
-                                border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
-                            ) {
-                                Text(text = getString(android.R.string.ok))
-                            }
-                        },
-                        dismissButton = {
-                            Button(
-                                onClick = {
-                                    showVenmoEmailDialog.value = false
-                                },
-                                backgroundColor = Color.Transparent,
-                                elevation = 0.dp,
-                                border = BorderStroke(1.dp, MaterialTheme.colors.secondary)
-                            ) {
-                                Text(text = getString(id = android.R.string.cancel))
-                            }
-                        },
-                    )
-                }
-
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.background(MaterialTheme.colors.surface),
+            ) {
                 Button(
                     onClick = {
                         showVenmoEmailDialog.value = true
                     },
-                    modifier = Modifier.layoutId("request_unlock")
-                        .padding(top = 8.dp, bottom = 8.dp),
+                    modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
                     shape = RoundedCornerShape(8.dp),
-                    backgroundColor = MaterialTheme.colors.secondary
+                    backgroundColor = MaterialTheme.colors.secondary,
                 ) {
                     Icon(
                         asset = Icons.Filled.LockOpen,
@@ -214,14 +133,12 @@ fun MainContent() {
                     )
                     Text(
                         text = getString(id = R.string.request_unlock),
-                        fontSize = TextUnit.Companion.Sp(36),
+                        fontSize = 36.sp,
                         modifier = Modifier.padding(8.dp),
                     )
                 }
 
-                Row(
-                    modifier = Modifier.layoutId("device_info"),
-                ) {
+                Row {
                     Column(
                         modifier = Modifier.weight(1f),
                         horizontalAlignment = Alignment.CenterHorizontally,
@@ -261,7 +178,7 @@ fun FAQDialogCard(info: FAQItem) {
     val showDialog = remember {
         mutableStateOf(false)
     }
-    
+
     Box(
         modifier = Modifier.padding(0.dp, 8.dp, 0.dp, 8.dp),
     ) {
@@ -338,6 +255,72 @@ fun FAQDialog(info: FAQItem, state: MutableState<Boolean>) {
                     }
                 }
             }
+        )
+    }
+}
+
+@Composable
+fun VenmoDialog(showVenmoEmailDialog: MutableState<Boolean>) {
+    val context = ContextAmbient.current
+    val enteredEmail = remember {
+        mutableStateOf("")
+    }
+
+    if (showVenmoEmailDialog.value) {
+        AlertDialog(
+            onDismissRequest = {},
+            title = {
+                Text(
+                    getString(R.string.enter_venmo_email),
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                    ),
+                )
+            },
+            text = {
+                TextField(
+                    value = enteredEmail.component1(),
+                    keyboardType = KeyboardType.Email,
+                    onValueChange = {
+                        enteredEmail.value = it
+                    }
+                )
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        if (Patterns.EMAIL_ADDRESS.matcher(enteredEmail.value).matches()) {
+                            context.sendRequest(enteredEmail.value)
+                            enteredEmail.value = ""
+                            showVenmoEmailDialog.value = false
+                        } else {
+                            Toast.makeText(
+                                context,
+                                R.string.enter_venmo_email,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    },
+                    backgroundColor = Color.Transparent,
+                    elevation = 0.dp,
+                    border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
+                ) {
+                    Text(text = getString(android.R.string.ok))
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = {
+                        showVenmoEmailDialog.value = false
+                    },
+                    backgroundColor = Color.Transparent,
+                    elevation = 0.dp,
+                    border = BorderStroke(1.dp, MaterialTheme.colors.secondary)
+                ) {
+                    Text(text = getString(id = android.R.string.cancel))
+                }
+            },
         )
     }
 }
